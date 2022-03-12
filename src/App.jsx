851fe581d18aa63bb5f2b42bd3e993a24a9964fb
kvmvkxnt <kamron1007dada@gmail.com>
2022-03-12 15:21:31 +0500

    Boiler

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <main>
+            <button className="modal__button">Open with useState</button>
+            <button className="modal__button">Open with useRef</button>
+            <select className='modal__button'>
+                
+            </select>
+        </main>
+    )
+}
+
+export default App;
